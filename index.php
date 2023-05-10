4e5c36cacacbdc1253a9cc1375ac354a89c92226
Ilcho Vuchkov <ilchovuchkov@gmail.com>
2023-05-10 17:11:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
-<DOCTYPE !html>
+<?php
+  $str = 'Hello programmer!';
+  $points = [
+    'Tools',
+    'Environments',
+    'Programming languages'
+  ]
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Hello PHP!</title>
+  <title><?php echo $str; ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    body {
+      text-align: center;
+      width: 100%;
+      margin:auto;
+    }
+  </style>
 </head>
 <body>
-  <h1>Hello PHP!</h1>
+  <h1><?php echo $str; ?></h1>
 </body>
 </html>
 
