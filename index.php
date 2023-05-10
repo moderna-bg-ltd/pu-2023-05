473c56114478cb394530b4379dd00e39d8737bbd
Ilcho Vuchkov <ilchovuchkov@gmail.com>
2023-05-10 17:21:08 +0300

    Add W3CSS and update index.php

--- index.php
+++ index.php
@@ -13,14 +13,23 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <style>
     body {
-      text-align: center;
       width: 100%;
       margin:auto;
     }
+    h1: {
+      text-align: center;
+    }
   </style>
+  <link rel="stylesheet" href="css/w3.css">
 </head>
 <body>
   <h1><?php echo $str; ?></h1>
+  <ul>
+  <?php
+  foreach ($points as $point) {
+    echo '<li>' . $point . '</li>';
+  }
+  ?>
+  </ul>
 </body>
 </html>
-
